292b7231141f163d5e88b15ca8fa0511b501814a
sammy171207 <sameerrandive558@gmail.com>
2025-01-08 08:47:34 +0530

    Recursion on string and remove the element using substring and without idx of sting

--- src/Recursion/String/RemoveElementOccurrence.java
+++ src/Recursion/String/RemoveElementOccurrence.java
@@ -2,6 +2,20 @@ package Recursion.String;
 
 public class RemoveElementOccurrence {
 
+
+    static String removeOcc3(String str,char ch){
+
+        if(str.length()==0){
+            return " ";
+        }
+        String smallans=removeOcc3(str.substring(1),ch);
+        char currChar=str.charAt(0);
+        if(currChar!=ch){
+            return currChar+smallans;
+        }else{
+            return smallans;
+        }
+    }
     static String removeOcc(String str,int idx,char ch){
 
         if(idx>=str.length()){
@@ -20,5 +34,7 @@ public class RemoveElementOccurrence {
    char ch='e';
   String print= removeOcc(s,0,ch);
   System.out.println(print);
+  String out=removeOcc3(s,ch);
+        System.out.println(out);
     }
 }
