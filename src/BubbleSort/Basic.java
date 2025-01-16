4e43a9ce025204a1e7f5e661b625c98339e69b93
sammy171207 <sameerrandive558@gmail.com>
2025-01-16 19:45:25 +0530

    reverse the sentence using stringbuilder and splits

--- src/BubbleSort/Basic.java
+++ src/BubbleSort/Basic.java
@@ -7,18 +7,24 @@ public class Basic {
     static int [] sort(int []arr){
         int n=arr.length;
         for(int i=0;i<n-1;i++){
+            boolean flag=false;
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    flag=true;
                 }
+            }
+            if(!flag){
+                //have swap happen
+                return arr;
             }
         }
         return arr;
     }
     public static void main(String[] args) {
-        int[] arr = {5, 4, 1, 7, 9, 2};
+        int[] arr = {1,2,3,4,5,6};
         int[] ans = sort(arr);
         System.out.println(Arrays.toString(ans));
 
