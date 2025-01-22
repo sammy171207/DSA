36787d08b3a48304f758b962478e5fc03c8db664
sammy171207 <sameerrandive558@gmail.com>
2025-01-22 22:59:38 +0530

    mergeSort Basic Logic

--- src/Sort/MergeSort/Basic.java
+++ src/Sort/MergeSort/Basic.java
@@ -1,15 +1,59 @@
 package Sort.MergeSort;
 
 public class Basic {
-    static void mergeSort(int []arr,int l, int r){
-        if(l>=r) return;
-        int mid=Math.abs(l+r/2);
-        mergeSort(arr,l,mid);
-        mergeSort(arr,mid+1,r);
-    }
-    public static void main(String[] args) {
-   int []arr={4,1,3,5,2};
-   int n=arr.length;
+    static void merge(int []arr, int l, int mid , int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
 
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            left[i] = arr[l + i];
+        for (int j = 0; j < n2; j++)
+            right[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void mergeSort(int []arr, int l, int r) {
+        if (l < r) {
+            int mid = l + (r - l) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        int []arr = {4, 1, 3, 5, 2};
+        mergeSort(arr, 0, arr.length - 1);
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println(0+arr.length-2/2);
     }
 }
