685031daa6304ea7a71f6883f73e03d2ce64b4ec
sammy171207 <sameerrandive558@gmail.com>
2025-01-23 21:46:23 +0530

    App One more Approach

--- src/Arrays/SecondLargestDigit.java
+++ src/Arrays/SecondLargestDigit.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class SecondLargestDigit {
     static int max(int[] arr) {
         int max = Integer.MIN_VALUE;
@@ -21,10 +23,25 @@ public class SecondLargestDigit {
         return max(arr);
     }
 
+    static int secondapp(int []arr){
+        Arrays.sort(arr);
+        int n = arr.length - 1;
+        int max = arr[n];
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] < max) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+
+
     public static void main(String[] args) {
-        int[] ar = {0,-2,0,-3,-4};
-        int ans=second(ar);
-        System.out.println(ans);
+//        int[] ar = {10, 10, 10};
+//        int ans=secondapp(ar);
+//        System.out.println(ans);
+
     }
 }
 
