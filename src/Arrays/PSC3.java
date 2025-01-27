f8d85a2595ae0a97ab6d57e7d1d4aa2361f869fc
sammy171207 <sameerrandive558@gmail.com>
2025-01-27 17:48:48 +0530

    Problem of GOG

--- src/Arrays/PSC3.java
+++ src/Arrays/PSC3.java
@@ -24,10 +24,27 @@ public class PSC3 {
        ans.add(-1);
        return ans;
     }
+
+        static ArrayList<Integer> approach2(int []arr,int target){
+            ArrayList<Integer>result=new ArrayList<>();
+            for(int i=0;i<arr.length;i++){
+                int sum=0;
+                for(int j=i;j<arr.length;j++){
+                    sum+=arr[j];
+                    if(sum==target){
+                        result.add(i+1);
+                        result.add(j+1);
+                        return result;
+                    }
+                }
+            }
+            result.add(-1);
+            return result ;
+        }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 7, 5};
         int target=12;
-        ArrayList<Integer> ans = subarraysum(arr,target);
+        ArrayList<Integer> ans = approach2(arr,target);
          for(int x:ans){
              System.out.print(x+" ");
          }
