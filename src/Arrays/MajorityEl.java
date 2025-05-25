83536e28cb3bd10ac8864c65afaeef595c58430e
sammy171207 <sameerrandive558@gmail.com>
2025-05-25 16:39:16 +0530

    MajorityEl

--- src/Arrays/MajorityEl.java
+++ src/Arrays/MajorityEl.java
@@ -27,10 +27,40 @@ public class MajorityEl {
          }
          return -1;
     }
-    public static void main(String[] args) {
-        int []arr={7};
 
-         int els= malel(arr);
-        System.out.println(els);
+  /// Boyer Moore Alogrithms remember it
+    static int optiomal(int[]arr){
+        int el = 0;
+        int cnt=0;
+        for(int i=0;i<arr.length;i++){
+            if(cnt==0){
+                el=arr[i];
+                cnt=1;
+            }
+            if(el==arr[i]){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+
+        int cnt1=0;
+        for(int i=0;i<arr.length;i++){
+            if(el==arr[i]){
+                cnt1++;
+            }
+        }
+        return cnt1>arr.length/2?el:0;
+    }
+    public static void main(String[] args) {
+        int []arr={2,2,1,1,1,2,2};
+        int []arr1={3,2,3};
+        int []arr3={};
+         int ans=optiomal(arr);
+         int ans2=optiomal(arr1);
+        System.out.println(ans);
+        System.out.println(ans2);
+        System.out.println(optiomal(arr3));
+
     }
 }
