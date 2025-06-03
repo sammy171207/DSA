ceb506ca94aff92a53591e1c3aef3bb8933ea0fb
sammy171207 <sameerrandive171207@gmail.com>
2025-06-03 21:25:43 +0530

    Piyush Fighting Club Where We can Find the how many Solider he can defended and sum the power of all the soldier which he defended

---
+++ src/BinarySearch/FightClub.java
@@ -0,0 +1,40 @@
+package BinarySearch;
+
+public class FightClub {
+    public static void main(String[] args) {
+        int[] soldiers = {1, 2, 3, 4, 5, 6, 7};
+        int[] piyushPower = {3, 10};
+
+        for (int i = 0; i < piyushPower.length; i++) {
+            fight(soldiers, piyushPower[i]);
+        }
+    }
+
+    private static int sumPower(int[] soldiers, int lastIndex) {
+        int sum = 0;
+        for (int i = 0; i <= lastIndex; i++) {
+            sum += soldiers[i];
+        }
+        return sum;
+    }
+
+    private static void fight(int[] soldiers, int power) {
+        int left = 0, right = soldiers.length - 1;
+        int lastDefeatedIndex = -1;  // No soldier defeated yet
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (soldiers[mid] <= power) {
+                lastDefeatedIndex = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        int count = lastDefeatedIndex + 1;
+        int totalPower = (lastDefeatedIndex >= 0) ? sumPower(soldiers, lastDefeatedIndex) : 0;
+
+        System.out.println(count + " " + totalPower);
+    }
+}
