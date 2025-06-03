f73f002037aa36dc4cb4daf51c5dc2abeda39bb9
sammy171207 <sameerrandive171207@gmail.com>
2025-06-03 20:37:45 +0530

    pascal triangle 3 possible problem first print the element of n row n col , print the entire row which ask for ,  print whole pascal triangle

--- src/Arrays/PascalTriangle.java
+++ src/Arrays/PascalTriangle.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.ArrayList;
+
 public class PascalTriangle {
 
     // this is help tp find the element present at col and row directly  in pascal triangle
@@ -12,17 +14,34 @@ public class PascalTriangle {
         return res;
     }
     // this function print the  number of row which tell
-    static void printRow(int c ,int r){
-        int ans=1;
-        System.out.println(ans);
-        for(int i=1;i<c;i++){
-            ans*=(c-i);
-            ans/=i;
-            System.out.println(ans);
+    static ArrayList<Integer> printRow(int r) {
+        int ans = 1;
+        ArrayList<Integer> rowpas = new ArrayList<>();
+        rowpas.add(ans);
+
+        for (int i = 1; i < r; i++) {
+            ans *= (r - i);
+            ans /= i;
+            rowpas.add(ans);
         }
-        System.out.println(1);
+
+        return rowpas;
+    }
+
+    static ArrayList<ArrayList<Integer>> generateTriangle(int n) {
+        ArrayList<ArrayList<Integer>> triangle = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            ArrayList<Integer> temp = printRow(i); // Corrected function call
+            triangle.add(temp);
+        }
+        return triangle;
     }
     public static void main(String[] args) {
+        int n = 5; // Example number of rows
+        ArrayList<ArrayList<Integer>> pascalTriangle = generateTriangle(n);
 
+        for (ArrayList<Integer> row : pascalTriangle) {
+            System.out.println(row);
+        }
     }
 }
