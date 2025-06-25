ad7a076ebabb4486e4df4c7ea6032a630e7eafcb
sammy171207 <sameerrandive171207@gmail.com>
2025-06-25 09:01:45 +0530

    Power Using Recursion

--- src/Recursion/PrintArrayRec.java
+++ src/Recursion/PrintArrayRec.java
@@ -30,11 +30,21 @@ public class PrintArrayRec {
         sumOfArray(arr,idx+1,sum);
 
     }
+
+    static int power(int a, int b){
+        if(b==0)return 1;
+        if(b%2==0)
+          return power(a, (int) Math.floor(b/2))*power(a,(int)Math.floor(b/2));
+        else
+            return a*power(a,(int)Math.floor(b/2))*power(a,(int)Math.floor(b/2));
+    }
     public static void main(String[] args) {
 
     int [] arr={1,2,3,4,6,7};
     print(arr,0);
     maxValue(arr,0,0);
     sumOfArray(arr,0,0);
+        System.out.println( power(2,5));
+
     }
 }
