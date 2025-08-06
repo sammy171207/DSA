eae8857df14675ed551f61c51068bb00246eff46
sammy171207 <sameerrandive171207@gmail.com>
2025-08-06 21:19:52 +0530

    All Possible Combination that can be formed by arr

--- src/Recursion/BackTracking/AllPossibleArray.java
+++ src/Recursion/BackTracking/AllPossibleArray.java
@@ -14,11 +14,31 @@ public class AllPossibleArray {
             current.remove(current.size()-1);
             subsequence(idx+1,arr,current,result);
     }
+
+    static  void allPossiblefromn(int idx,int n,List<Integer>current,List<List<Integer>>result){
+            if(idx==n){
+                result.add(new ArrayList<>(current));
+                return;
+            }
+            current.add(idx);
+            allPossiblefromn(idx+1,n,current,result);
+            current.remove(current.size()-1);
+            allPossiblefromn(idx+1,n,current,result);
+    }
     public static void main(String[] args) {
-        int[]arr={1,2,3};
+//        int[]arr={1,2,3};
         List<Integer>subarr=new ArrayList<>();
         List<List<Integer>>result=new ArrayList<>();
-        subsequence(0,arr,subarr,result);
+
+//        subsequence(0,arr,subarr,result);
+        int n=3;
         System.out.println(result.toString());
+        allPossiblefromn(0,n,subarr,result);
+
+        for(List<Integer>li:result){
+            System.out.println(li);
+        }
+
+        System.out.println();
     }
 }
